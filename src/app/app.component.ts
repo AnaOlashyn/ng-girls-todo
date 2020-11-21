27b745153762e3d11c745a1623ad3280f9b179de
AnaOlashyn <anaolashyn@gmail.com>
2020-11-21 13:01:51 +0100

    add output functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,18 @@ import { TodoItem } from './interfaces/todo-item';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'todo-list';
   todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
   ];
+
+  addItem(value: string) {    
+    this.todoList.push({ title: value });
+  }
 }
