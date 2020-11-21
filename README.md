c320341f8dae0b3afedca660fb3288d6c92e7f9e
AnaOlashyn <anaolashyn@gmail.com>
2020-11-21 15:55:10 +0100

    fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ng-girls-todo
+# ngGirlsProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-girls-todo'`, () => {
+  it(`should have as title 'ngGirlsProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-girls-todo');
+    expect(app.title).toEqual('ngGirlsProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-girls-todo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ngGirlsProject app is running!');
   });
 });
