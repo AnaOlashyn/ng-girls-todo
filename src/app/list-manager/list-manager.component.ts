d69de47076a8a41397a9c3173ddcc970b40b16a1
AnaOlashyn <anaolashyn@gmail.com>
2020-11-21 14:25:24 +0100

    add checkbox and update item

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -25,4 +25,7 @@ export class ListManagerComponent implements OnInit {
     this.todoListService.deleteItem(item);
   }
 
+  updateItem(item, changes) {
+    this.todoListService.updateItem(item, changes);
+  }
 }
